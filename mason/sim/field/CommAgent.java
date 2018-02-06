f1fdecbe697b28abfefcb32b8655dfc7b99e7cb3
nosyndicate <nosyndicate@gmail.com>
2018-02-05 19:30:37 -0500

    add raw type migration

---
+++ mason/sim/field/CommAgent.java
@@ -0,0 +1,23 @@
+package sim.field;
+
+import java.io.ObjectInputStream;
+
+import sim.field.DObjectMigrator.AgentOutputStream;
+
+public class CommAgent implements Cloneable
+{
+	public void writePrimitiveTypeData(AgentOutputStream out)
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public void readPrimitiveTypeData(ObjectInputStream in)
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public Object clone()
+	{
+		throw new UnsupportedOperationException();
+	}
+}
