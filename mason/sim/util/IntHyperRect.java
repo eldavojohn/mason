47ea03c6fa298cbf1b90e290e5dc8621c86ae157
Haoliang Wang <haoliangx@users.noreply.github.com>
2018-03-01 16:49:09 -0500

    Fix bug in IsIntersect() in IntHyperRect class Add intersect() to generate a new IntHyperRect object representing the intersection of two hyperrectangles

--- mason/sim/util/IntHyperRect.java
+++ mason/sim/util/IntHyperRect.java
@@ -28,11 +28,11 @@ public class IntHyperRect implements Comparable<IntHyperRect> {
 		return IntStream.range(0, p.nd).allMatch(i -> ul.c[i] <= p.c[i] && p.c[i] < br.c[i]);
 	}
 
-	public boolean isOverlap(IntHyperRect that) {
+	public boolean isIntersect(IntHyperRect that) {
 		if (this.nd != that.nd)
 			throw new IllegalArgumentException("Number of dimensions must be the same. Got " + this.nd + " and " + that.nd);
-		
-		return this.contains(that.ul) || this.contains(that.br) || that.contains(this.ul) || that.contains(this.br);
+
+		return IntStream.range(0, nd).allMatch(i -> this.ul.c[i] < that.br.c[i] && this.br.c[i] > that.ul.c[i]);
 	}
 
 	public int[] getRelPos(IntHyperRect that) {
@@ -42,6 +42,19 @@ public class IntHyperRect implements Comparable<IntHyperRect> {
 		return IntStream.range(0, this.nd)
 		       .map(i -> this.ul.c[i] >= that.br.c[i] ? -1 : (that.ul.c[i] >= this.br.c[i]) ? 1 : 0)
 		       .toArray();
+	}
+
+	public IntHyperRect intersect(IntHyperRect that) {
+		if (this.nd != that.nd)
+			throw new IllegalArgumentException("Number of dimensions must be the same. Got " + this.nd + " and " + that.nd);
+
+		if (!isIntersect(that))
+			throw new IllegalArgumentException("Given " + that + " does not intersect with this " + this);
+
+		int[] c1 = IntStream.range(0, nd).map(i -> Math.max(this.ul.c[i], that.ul.c[i])).toArray();
+		int[] c2 = IntStream.range(0, nd).map(i -> Math.min(this.br.c[i], that.br.c[i])).toArray();
+
+		return new IntHyperRect(-1, new IntPoint(c1), new IntPoint(c2));
 	}
 
 	public Segment getSegment(int dim) {
@@ -73,5 +86,21 @@ public class IntHyperRect implements Comparable<IntHyperRect> {
 
 	public String toString() {
 		return String.format("%s<%d, %s, %s>", this.getClass().getSimpleName(), id, ul.toString(), br.toString());
+	}
+
+	public static void main(String[] args) {
+		IntPoint p1 = new IntPoint(new int[] {1, 1});
+		IntPoint p2 = new IntPoint(new int[] {0, 3});
+		IntPoint p3 = new IntPoint(new int[] {4, 4});
+		IntPoint p4 = new IntPoint(new int[] {2, 6});
+		IntHyperRect r1 = new IntHyperRect(0, p1, p3);
+		IntHyperRect r2 = new IntHyperRect(1, p2, p4);
+
+		System.out.println(r1.intersect(r2));
+
+		IntPoint p5 = new IntPoint(new int[] {0, 4});
+		IntHyperRect r3 = new IntHyperRect(1, p5, p4);
+
+		System.out.println(r1.isIntersect(r3));
 	}
 }
