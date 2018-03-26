a601c0f8ba588523e1b4c2f0b403825f2251a15c
Haoliang Wang <haoliangx@users.noreply.github.com>
2018-03-26 13:23:53 -0400

    Update loadbalancer to use graph coloring and runtime to generate balance actions and add fakeHaloField in loadbalancer for testing

--- mason/sim/field/LoadBalancer.java
+++ mason/sim/field/LoadBalancer.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 import sim.util.IntHyperRect;
+import sim.util.GraphColoring;
 
 import mpi.*;
 
@@ -12,7 +13,10 @@ public class LoadBalancer {
 	DNonUniformPartition p;
 	HaloField f;
 
+	GraphColoring gc;
+
 	double threshold;
+	//int interval;
 
 	// Use aoi as the offset to adjust partitions
 	int[] aoi;
@@ -22,6 +26,9 @@ public class LoadBalancer {
 		this.f = f;
 		this.aoi = aoi;
 		this.threshold = threshold;
+		//this.interval = interval;
+
+		this.gc = new GraphColoring(p);
 	}
 
 	// Get all the neighbor ids and then
@@ -41,21 +48,23 @@ public class LoadBalancer {
 		return ret;
 	}
 
-	// TODO use graph coloring to allow multiple nodes to perform balancing at the same time
 	private boolean shouldBalance(int step) {
-		return step % p.np == p.pid ;
+		gc.color();
+		if (step % gc.numColors == gc.myColor)
+			return true;
+		return false;
 	}
 
-	private BalanceAction getAction() {
+	private BalanceAction getAction(int[][] avail) {
 		HashMap<Integer, Double> m = f.getRuntimes();
-		int[][] avail = getAvailNeighborIds();
 
 		int dim = 0, myPid = p.getPid(), target = myPid, offset = 0;
 		double myRt = m.get(myPid), maxDelta = 0;
+		int[] size = p.getPartition().getSize();
 
 		for ( int d = 0; d < p.nd; d++) {
 			for (int t : avail[d]) {
-				double delta = myRt - m.get(t);
+				double delta = (myRt - m.get(t)) * aoi[d] / size[d];
 				if (Math.abs(delta) > maxDelta) {
 					maxDelta = Math.abs(delta);
 					dim = d;
@@ -72,15 +81,9 @@ public class LoadBalancer {
 		return new BalanceAction(myPid, target, dim, offset * aoi[dim]);
 	}
 
-	// Now it is just randomly choosing targets
-	// TODO choose target to minimize the variance of runtime between this node and its available neighbors
+	// Randomly choosing targets
 	// TODO use the random number generator in MASON
-	private BalanceAction generateAction(int step) {
-		if (!shouldBalance(step))
-			return new BalanceAction();
-
-		int[][] avail = getAvailNeighborIds();
-
+	private BalanceAction getRandomAction(int[][] avail) {
 		java.util.Random r = new java.util.Random();
 
 		int[] nonEmptyIdxs = IntStream.range(0, p.nd).filter(x -> avail[x].length > 0).toArray();
@@ -92,6 +95,13 @@ public class LoadBalancer {
 		int offset = r.nextBoolean() ? aoi[dim] : -aoi[dim];
 
 		return new BalanceAction(p.pid, target, dim, offset);
+	}
+
+	private BalanceAction generateAction(int step) {
+		if (!shouldBalance(step))
+			return new BalanceAction();
+
+		return getAction(getAvailNeighborIds());
 	}
 
 	public void balance(int step) throws MPIException {
@@ -118,7 +128,7 @@ public class LoadBalancer {
 	}
 
 	public static void main(String args[]) throws MPIException, InterruptedException {
-		int[] size = new int[] {10, 10};
+		int[] size = new int[] {100, 100};
 		int[] aoi = new int[] {1, 1};
 
 		MPI.Init(args);
@@ -128,17 +138,51 @@ public class LoadBalancer {
 		p.initUniformly(null);
 		p.setMPITopo();
 
-		HaloField hf = new HaloField(p, aoi, p.pid);
+		FakeHaloField hf = new FakeHaloField(p, aoi, p.pid);
 		hf.sync();
 
-		LoadBalancer lb = new LoadBalancer(p, hf, aoi, 1000);
-		lb.printHaloField();
-		for (int i = 0; i < 5; i++) {
-			lb.balance(i);
-			lb.printHaloField();
-		}
+		LoadBalancer lb = new LoadBalancer(p, hf, aoi, 0);
+		// lb.printHaloField();
+		// for (int i = 0; i < 5; i++) {
+		// 	lb.balance(i);
+		// 	lb.printHaloField();
+		// }
+
+		lb.balance(0);
+		hf.setRuntimes(new double[]{200 , 100, 100, 50});
+		lb.balance(1);
+		//hf.setRuntimes(new double[]{ , , , });
+		lb.balance(2);
+		//hf.setRuntimes(new double[]{ , , , });
+		lb.balance(3);
 
 		MPI.Finalize();
+	}
+
+	static class FakeHaloField extends sim.field.HaloField {
+		public HashMap<Integer, Double> rt;
+
+		public FakeHaloField(DPartition ps, int[] aoi, double initVal) {
+			super(ps, aoi, initVal);
+			rt = new HashMap<Integer, Double>();
+			for (int i = 0; i < ps.getNumProc(); i++)
+				rt.put(i, 100.0);
+		}
+
+		public void setRuntimes(double[] rts) {
+			for (int i = 0; i < ps.getNumProc(); i++)
+				rt.put(i, rts[i]);
+		}
+
+		@Override
+		public HashMap<Integer, Double> getRuntimes() {
+			HashMap<Integer, Double> ret = new HashMap<Integer, Double>();
+			
+			ret.put(ps.getPid(), rt.get(ps.getPid()));
+			Arrays.stream(neighbors).forEach(x -> ret.put(x.pid, rt.get(x.pid)));
+
+			return ret;
+		}
 	}
 
 	private void printHaloField() throws MPIException, InterruptedException {
