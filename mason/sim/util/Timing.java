668b812914f3f894a9bcf5812c6b8e0f17b4b9c6
Haoliang Wang <haoliangx@users.noreply.github.com>
2018-04-09 17:16:59 -0400

    Remove init in Timing | Let others set fakeclocks in Timing for testing

--- mason/sim/util/Timing.java
+++ mason/sim/util/Timing.java
@@ -5,110 +5,109 @@ import java.util.NoSuchElementException;
 
 public class Timing {
 
-	// Used by load balancer to determine the workload on each node
-	public static final String LB_RUNTIME = "MASON_LOAD_BALANCING_RUNTIME";
-	// Used by load balancer to determine the overhead for each load balancing
-	public static final String LB_OVERHEAD = "MASON_LOAD_BALANCING_OVERHEAD";
+	public static final String LB_RUNTIME = "_MASON_LOAD_BALANCING_RUNTIME";
+	public static final String LB_OVERHEAD = "_MASON_LOAD_BALANCING_OVERHEAD";
+	public static final String MPI_SYNC_OVERHEAD = "_MASON_MPI_SYNC_OVERHEAD";
 
-	public static final String MPI_SYNC_OVERHEAD = "MASON_MPI_SYNC_OVERHEAD";
-
-	private static int cap;
-	private static HashMap<String, TimingStat> m;
-	private static NanoClock clock;
+	private static int window = 100;
+	private static HashMap<String, TimingStat> m = new HashMap<String, TimingStat>();
+	private static NanoClock clock = new NanoClock() {
+		public long nanoTime() { return System.nanoTime(); }
+		public void advance(long val) { throw new UnsupportedOperationException("Cannot set nano time for real clock"); }
+	};
 
 	private static class FakeClock implements NanoClock {
-		public long val;
+		public long val = 0;
 		public long nanoTime() { return val; }
+		public void advance(long val) { this.val += val; }
 	}
 
 	private interface NanoClock {
 		long nanoTime();
+		void advance(long val);
 	}
 
-	// TODO allow multiple init? or do we need to init at all?
-	public static void init(int capacity) {
-		cap = capacity;
-		m = new HashMap<String, TimingStat>();
-		clock = new NanoClock() { public long nanoTime() { return System.nanoTime(); } };
+	public static void useFakeClock() {
+		clock = new FakeClock();
 	}
 
-	public static void initMetrics(String ... ids) {
-		for (String id : ids)
-			if (m.containsKey(id))
-				throw new IllegalArgumentException("Timer for " + id + " already exists");
-			else
-				m.put(id, new TimingStat(cap));
+	public static void advanceFakeClock(long time) {
+		clock.advance(time);
+	}
+
+	public static void advanceFakeClockMS(int time) {
+		clock.advance(time * 1000000L);
+	}
+
+	public static void setWindow(int win) {
+		window = win;
 	}
 
 	public static void start(String ... ids) {
-		for (String id : ids)
-			if (!m.containsKey(id))
-				throw new NoSuchElementException("Timer for " + id + " does not exist");
-			else
-				m.get(id).start(clock.nanoTime());
+		for (String id : ids) {
+			m.putIfAbsent(id, new TimingStat(window));
+			m.get(id).start(clock.nanoTime());
+		}
 	}
 
 	public static void stop(String ... ids) {
-		for (String id : ids)
-			if (!m.containsKey(id))
-				throw new NoSuchElementException("Timer for " + id + " does not exist");
-			else
-				m.get(id).stop(clock.nanoTime());
+		for (String id : ids) {
+			check(id);
+			m.get(id).stop(clock.nanoTime());
+		}
 	}
 
 	public static void reset(String ... ids) {
-		for (String id : ids)
-			if (!m.containsKey(id))
-				throw new NoSuchElementException("Timer for " + id + " does not exist");
-			else
-				m.get(id).reset();
+		for (String id : ids) {
+			check(id);
+			m.get(id).reset();
+		}
 	}
 
 	public static TimingStat get(String id) {
-		if (m == null || !m.containsKey(id))
-			throw new NoSuchElementException("Timer for " + id + " does not exist or Timing not init");
+		check(id);
 		return m.get(id);
 	}
 
+	private static void check(String id) {
+		if (!m.containsKey(id))
+			throw new NoSuchElementException("Timer for " + id + " does not exist");
+	}
+
 	public static void main(String[] args) {
-		init(3);
-		initMetrics("Test1", "Test2");
-		
-		FakeClock fakeClock = new FakeClock();
-		clock = fakeClock;
+		useFakeClock();
 
 		System.out.println("Name  \tCount \tMinimum \tMaximum \tOverall Mean \tOverall Stdev \tMoving Average \tMoving Stdev \tUnit");
-		fakeClock.val = 0L;
 
 		start("Test1");
-		fakeClock.val += 5000000L;
+		advanceFakeClock(5000000L);
 		stop("Test1");
 		System.out.println("Test1\t" + get("Test1"));
 
 		start("Test1");
-		fakeClock.val += 6000000L;
+		advanceFakeClock(6000000L);
 		stop("Test1");
 		System.out.println("Test1\t" + get("Test1"));
 
 		start("Test1");
-		fakeClock.val += 7000000L;
+		advanceFakeClock(7000000L);
 		stop("Test1");
 		System.out.println("Test1\t" + get("Test1"));
 
 		start("Test1");
-		fakeClock.val += 3000000L;
+		advanceFakeClock(3000000L);
 		stop("Test1");
 		System.out.println("Test1\t" + get("Test1"));
 
 		start("Test1");
-		fakeClock.val += 4000000L;
+		advanceFakeClock(4000000L);
 		stop("Test1");
 		System.out.println("Test1\t" + get("Test1"));
 
 		start("Test1", "Test2");
-		fakeClock.val += 4000000L;
+		advanceFakeClock(4000000L);
 		stop("Test1");
-		fakeClock.val += 4000000L;
+		advanceFakeClock(4000000L);
 		stop("Test2");
 		System.out.println("Test1\t" + get("Test1"));
 		System.out.println("Test2\t" + get("Test2"));
@@ -116,19 +115,19 @@ public class Timing {
 		reset("Test1");
 
 		start("Test1", "Test2");
-		fakeClock.val += 4000000L;
+		advanceFakeClock(4000000L);
 		stop("Test2", "Test1");
 		System.out.println("Test1\t" + get("Test1"));
 		System.out.println("Test2\t" + get("Test2"));
 
 		start("Test1", "Test2");
-		fakeClock.val += 100000000L;
+		advanceFakeClock(100000000L);
 		stop("Test2", "Test1");
 		System.out.println("Test1\t" + get("Test1"));
 		System.out.println("Test2\t" + get("Test2"));
 
 		start("Test1", "Test2");
-		fakeClock.val += 40000000L;
+		advanceFakeClock(40000000L);
 		stop("Test2", "Test1");
 		System.out.println("Test1\t" + get("Test1"));
 		System.out.println("Test2\t" + get("Test2"));
